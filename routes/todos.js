23d469a462b12af10cf2459ca501d33505fd8782
Alexander Rowland <arowland900@gmail.com>
2019-02-12 09:38:24 -0800

    todosController

--- routes/todos.js
+++ routes/todos.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var Todo = require('../models/todo')
+var todosController = require('../controllers/todos')
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.render('todos/index', {
-    todos: Todo.getAll()
-  });
-});
+router.get('/', todosController.index);
 
 module.exports = router;
